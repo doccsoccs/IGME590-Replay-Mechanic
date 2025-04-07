f16492ac908e1c79bd2cb5148208132ee41cdfce
doccsoccs <davinhow2@gmail.com>
2025-04-07 14:48:12 -0400

    fixed gravity for the enemy

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,19 +1,16 @@
 extends CharacterBody2D
 
 var pos_record: Array[Vector2] = []
-var rewinding: bool
+var rewinding: bool = false
 
 @export_category("COLORS")
-@export var normal_color: Color = Color.WHITE
-@export var rewind_color: Color = Color.REBECCA_PURPLE
+@export var normal_color: Color = Color.RED
+@export var rewind_color: Color = Color.WEB_PURPLE
 
 @export_category("MOVEMENT")
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
+@export var SPEED = 100.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction
 
 func _physics_process(delta):
 	if !rewinding:
@@ -21,20 +18,13 @@ func _physics_process(delta):
 		# Add the gravity.
 		if not is_on_floor():
 			velocity.y += gravity * delta
-
-		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-
+		
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
-		direction = Input.get_axis("ui_left", "ui_right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+		var direction = Vector2(1,0).normalized()
+		velocity.x = direction.x * SPEED
+		#position += velocity
 		move_and_slide()
-	
 	else:
 		rewind()
 
--- Scripts/replay.gd
+++ Scripts/replay.gd
@@ -2,16 +2,16 @@ extends Node
 
 var recording: bool = true
 var position_record: Array[Vector2] = []
-@onready var player = $".."
+@onready var entity = $".."
 
 func _process(_delta):
 	if recording:
-		position_record.push_back(player.position)
+		position_record.push_back(entity.position)
 
 func _input(event):
 	if event.is_action_pressed("Rewind"):
 		recording = false
-		player.init_rewind(position_record)
+		entity.init_rewind(position_record)
 	elif event.is_action_released("Rewind"):
-		player.end_rewind()
+		entity.end_rewind()
 		recording = true
